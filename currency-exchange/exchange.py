e2f09d1f6fa14d3a3e9dac6e66af9343faca0a4e
Irench1k <irena.yarlyk@gmail.com>
2023-08-04 17:18:32 +0200

    solved exchange problem

--- currency-exchange/exchange.py
+++ currency-exchange/exchange.py
@@ -1,33 +1,30 @@
-def exchange_money(budget, exchange_rate):
+def exchange_money(budget: float, exchange_rate: float) -> float:
     return budget / exchange_rate
-    
 
 
-def get_change(budget, exchanging_value):
+def get_change(budget: float, exchanging_value: float) -> float:
     left_money = budget - exchanging_value
     return left_money
 
 
-def get_value_of_bills(denomination, number_of_bills):
+def get_value_of_bills(denomination: float, number_of_bills: float) -> int:
     total_bills = int(denomination * number_of_bills)
     return total_bills
 
 
-def get_number_of_bills(budget, denomination):
+def get_number_of_bills(budget: float, denomination: float) -> float:
     number_of_currency_bills = budget // denomination
     return number_of_currency_bills
 
 
-def get_leftover_of_bills(budget, denomination):
+def get_leftover_of_bills(budget: float, denomination: float) -> float:
     leftover = budget % denomination
     return leftover
-    
 
 
-def exchangeable_value(budget, exchange_rate, spread, denomination):
-    exchange_fee = spread / 100
-    exchange_rate_with_spread = exchange_rate * (1 + spread_decimal)
-    exchanged_currency_value = budget * exchange_rate_with_spread
-    max_exchanged_currency_value = int(exchanged_currency_value // denomination) * denomination
-    
-    return max_exchanged_currency_value 
+def exchangeable_value(budget: float, exchange_rate: float, spread: int, denomination: int) -> int:
+    new_exchange_rate = (exchange_rate * (spread / 100)) + exchange_rate
+    total_budget = budget / new_exchange_rate
+    total_currency_denmination = total_budget // denomination
+    result = int(total_currency_denmination * denomination)
+    return result
