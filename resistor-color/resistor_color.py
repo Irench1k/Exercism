377432ccdae6335ac045f86c96b2663605d5fe82
Irench1k <irena.yarlyk@gmail.com>
2023-08-08 17:29:32 +0200

    smarter solution for resistor-color problem

--- resistor-color/resistor_color.py
+++ resistor-color/resistor_color.py
@@ -1,6 +1,21 @@
-def color_code(color):
-    pass
+color_values = {
+        "black": 0,
+        "brown": 1,
+        "red": 2,
+        "orange": 3,
+        "yellow": 4,
+        "green": 5,
+        "blue": 6,
+        "violet": 7,
+        "grey": 8,
+        "white": 9,
+    }
+
+
+def color_code(color: str) -> int:
+    return color_values[color]
 
 
 def colors():
-    pass
+    colors = list(color_values.keys())
+    return colors
