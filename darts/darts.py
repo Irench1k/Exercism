322d9ac1e4af7ffff9b70a7b0737665556680b5b
Irench1k <irena.yarlyk@gmail.com>
2023-08-14 18:47:05 +0200

    solved darts problem

---
+++ darts/darts.py
@@ -0,0 +1,13 @@
+import math
+
+
+def score(x, y):
+    hypotenuse = math.sqrt(x**2 + y**2)
+    if hypotenuse > 10:
+        return 0
+    if 10 >= hypotenuse > 5:
+        return 1
+    if 5 >= hypotenuse > 1:
+        return 5
+    else:
+        return 10
