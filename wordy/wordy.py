8a0f776043b83d58d68673bc22d3a82a2ccb799e
Irench1k <irena.yarlyk@gmail.com>
2023-08-18 20:47:19 +0200

    finished this shitty problem!

--- wordy/wordy.py
+++ wordy/wordy.py
@@ -1,3 +1,7 @@
+from typing import Callable
+ListOfOperations = list[tuple[Callable[[int, int], int], int]]
+
+
 def plus(a: int, b: int) -> int:
     return a + b
 
@@ -14,62 +18,77 @@ def divide(a: int, b: int) -> int:
     return a // b
 
 
-operations = {
-    'plus': plus,
-    'minus': minus,
-    'multiplied by': multiply,
-    'divided by': divide
-}
+def answer(question: str) -> int:
+    '''Main function which is called by Exercism test suite.'''
+    first_term, action_string = checking(question)
+    try:
+        operators_and_numbers = action_list(action_string)
+    except:
+        raise ValueError("syntax error")
+    
+    result = execute(first_term, operators_and_numbers)
+    return result
 
 
-def answer(question: str):
+def checking(question: str) -> tuple[int, str]:
+    """Checks the question and convert it into a tuple of the first int and rest as a string"""
     if not question.startswith('What is') or 'cubed' in question:
         raise ValueError("unknown operation")
 
     question = question.removeprefix("What is").removesuffix("?").strip()
     list_question = question.split()
 
-    result = int(list_question[0])
-    i = 1
-    while i < len(list_question):
-        number = result
-        operator = list_question[i]
-        next_number = int(list_question[i + 1])
+    try:
+        first_term = int(list_question[0])
+    except:
+        raise ValueError("syntax error")
 
-        if operator in operations:
-            result = operations[operator](number, next_number)
-            print(f"The result is: {result}")
+    action_string = ' '.join(list_question[1:])
+
+    return first_term, action_string
+
+
+def action_list(action_string: str) -> ListOfOperations:
+    """('plus 5 minus 10') -> [(plus, 5), (minus, 10)]"""
+    words = action_string.split()
+    result: ListOfOperations = []
+    i = 0
+
+    while i < len(words):
+
+        if words[i] == 'plus':
+            action = plus
+            value = int(words[i + 1])
+            result.append((action, value))
+            processed = 2
+        elif words[i] == 'minus':
+            action = minus
+            value = int(words[i + 1])
+            result.append((action, value))
+            processed = 2
+        elif words[i] == 'multiplied' and words[i + 1] == 'by':
+            action = multiply
+            value = int(words[i + 2])
+            result.append((action, value))
+            processed = 3
+        elif words[i] == 'divided' and words[i + 1] == 'by':
+            action = divide
+            value = int(words[i + 2])
+            result.append((action, value))
+            processed = 3
         else:
             raise ValueError("syntax error")
-        i += 2
-        
-    return result
-
-
-print(answer('What is 5 plus 5 plus -12?'))
-
-
-
-def preprocess(input_string: str) -> tuple[int, str]:
-    pass
-
-initial_value, action_string = preprocess(input_string)
-
-#initial_value = 5
-#action_string = "multiple by 2 plus 6"
-
-actions = action_list(action_string)
-
-#actions = [("multiple by", 2), ("plus", 6)]
-#actions = [(multiply, 2), (plus, 6)]
-
-
-result = execute(initial_value, actions)
-
-
-def execute(initial_value: int, actions: list[tuple]) -> int:
-    result = inital_value
-    for action_fn, value in actions:
-        result = action(result, value)
+        i += processed
 
     return result
+
+
+def execute(first_term: int, operators_and_numbers: ListOfOperations) -> int:
+    """Calculates the final answer"""
+    answer = first_term
+    for action, value in operators_and_numbers:
+        answer = action(answer, value)
+    return answer
+
+
+print(answer("What is plus 5 10?"))
